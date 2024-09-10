ac40622694e5bfd4c3c66f0b5aaa21df5cc159f4
xdite <9004+xdite@users.noreply.github.com>
2024-09-11 06:44:07 +0800

    偏右

--- background.js
+++ background.js
@@ -7,15 +7,36 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
+function openWindowOnRight(url) {
+  chrome.windows.getCurrent({}, (currentWindow) => {
+    const width = 1000;
+    const height = 800;
+    const top = currentWindow.top;
+    let left = currentWindow.left + currentWindow.width;
+
+    // 檢查是否超出屏幕右邊界
+    chrome.system.display.getInfo((displays) => {
+      const display = displays[0];  // 假設使用主顯示器
+      if (left + width > display.bounds.width) {
+        left = display.bounds.width - width;
+      }
+
+      chrome.windows.create({ 
+        url: url, 
+        type: 'popup',
+        width: width,
+        height: height,
+        top: top,
+        left: left
+      });
+    });
+  });
+}
+
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "sendToRagReader") {
     let videoUrl = info.linkUrl || tab.url;
     let targetUrl = `http://localhost:8501/?youtube_url=${encodeURIComponent(videoUrl)}`;
-    chrome.windows.create({ 
-      url: targetUrl, 
-      type: 'popup',
-      width: 1000,
-      height: 800
-    });
+    openWindowOnRight(targetUrl);
   }
 });
--- popup.js
+++ popup.js
@@ -1,14 +1,33 @@
+function openWindowOnRight(url) {
+  chrome.windows.getCurrent({}, (currentWindow) => {
+    const width = 1000;
+    const height = 800;
+    const top = currentWindow.top;
+    let left = currentWindow.left + currentWindow.width;
+
+    // 檢查是否超出屏幕右邊界
+    chrome.system.display.getInfo((displays) => {
+      const display = displays[0];  // 假設使用主顯示器
+      if (left + width > display.bounds.width) {
+        left = display.bounds.width - width;
+      }
+
+      chrome.windows.create({ 
+        url: url, 
+        type: 'popup',
+        width: width,
+        height: height,
+        top: top,
+        left: left
+      });
+    });
+  });
+}
+
 document.getElementById('sendUrl').addEventListener('click', function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     let currentUrl = tabs[0].url;
     let targetUrl = `http://localhost:8501/?youtube_url=${encodeURIComponent(currentUrl)}`;
-    
-    // 在新的獨立窗口中打開目標URL，設定寬度為1000像素
-    chrome.windows.create({ 
-      url: targetUrl, 
-      type: 'popup',
-      width: 1000,
-      height: 800
-    });
+    openWindowOnRight(targetUrl);
   });
 });
